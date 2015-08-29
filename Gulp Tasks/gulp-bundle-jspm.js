7065c10a837816db77c31e3dbca16f930ca4bcf3
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-08-29 23:06:43 +1000

    Bundling in JSPM now works appropriately. Dependencies are manually defined for now.

--- Gulp Tasks/gulp-bundle-jspm.js
+++ Gulp Tasks/gulp-bundle-jspm.js
@@ -1,20 +1,11 @@
-var jspm = require('jspm');
-var builder = jspm.Builder();
-
-/* Ignore dependencies */
-var ignoreDependencies = [
-  'traceur',
-  'traceur-runtime',
-  'babel',
-  'babel-runtime'
-];
-
-// Iterate all keys in config.js (map: {}) to get dependency names
-// This will form a dependency string that looks like:
-// 'aurelia-framework + aurelia-metadata + aurelia-metadata' and so on...
-var packages = Object.keys(builder.loader.map).filter(function(pkg) {
-    return ignoreDependencies.indexOf(pkg) === -1;
-}).join(' + ');
+/* Dependencies to install */
+var packages = [
+  "*",
+  "aurelia-bootstrapper",
+  "aurelia-dependency-injection",
+  "aurelia-http-client",
+  "aurelia-router"
+].join(" + ");
 
 gulp.task('bundle-js', function(done) {
   jspm.bundle(packages,
