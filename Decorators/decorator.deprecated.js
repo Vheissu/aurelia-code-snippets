ad93cc05cce527e2a5e9a90efe58cb5997ccfcde
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-08-30 00:24:36 +1000

    Added in @deprecated() decorator for marking methods as deprecated

---
+++ Decorators/decorator.deprecated.js
@@ -0,0 +1,18 @@
+export default function deprecated(...args) {
+  return function decorator(target, key, descriptor) {
+    let msg = 'This method is deprecated and will be removed in a future version.';
+
+    if (typeof descriptor.value !== 'function') {
+        throw new SyntaxError('Methods can only be marked as deprecated.');
+    }
+
+    let contextual = `${target.constructor.name}#${key}`;
+    return {
+      ...descriptor,
+      value: function methodWrapper() {
+          console.warn(`DEPRECATED ${contextual}: ${msg}`);
+          return descriptor.value.apply(this, arguments);
+      }
+    }
+  }
+}
