28b4ab398bc692bd3caf949f9045a71679bc8347
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-08-30 00:30:04 +1000

    Deprecated decorator accepts content as its first and only checked argument.

--- Decorators/decorator.deprecated.js
+++ Decorators/decorator.deprecated.js
@@ -1,18 +1,23 @@
 export default function deprecated(...args) {
-  return function decorator(target, key, descriptor) {
-    let msg = 'This method is deprecated and will be removed in a future version.';
+    return function decorator(target, key, descriptor) {
+      let msg = 'This method is deprecated and will be removed in a future version.';
 
-    if (typeof descriptor.value !== 'function') {
-        throw new SyntaxError('Methods can only be marked as deprecated.');
-    }
+      if (typeof descriptor.value !== 'function') {
+          throw new SyntaxError('Methods can only be marked as deprecated.');
+      }
 
-    let contextual = `${target.constructor.name}#${key}`;
-    return {
-      ...descriptor,
-      value: function methodWrapper() {
-          console.warn(`DEPRECATED ${contextual}: ${msg}`);
-          return descriptor.value.apply(this, arguments);
+      if (args[0]) {
+          msg += `\n\n ${args[0]}`;
+      }
+
+      let contextual = `${target.constructor.name}#${key}`;
+
+      return {
+        ...descriptor,
+        value: function methodWrapper() {
+            console.warn(`DEPRECATED ${contextual}: ${msg}`);
+            return descriptor.value.apply(this, arguments);
+        }
       }
     }
-  }
 }
